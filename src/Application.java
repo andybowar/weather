ce85268667c8fa26ad2b0448535e770caa2d50cc
c5403 <james.p.mcshane@healthpartners.com>
2018-04-21 08:56:51 -0500

    Component scan oddities

--- src/Application.java
+++ src/Application.java
@@ -1,12 +1,13 @@
 import CurrentWeather.WeatherCat;
 import WeatherStation.FindWeatherStation;
+import WeatherStation.WeatherApplicationRunner;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = WeatherApplicationRunner.class)
 public class Application {
 
     public static void main(String[] args) {
--- src/WeatherStation/WeatherApplicationRunner.java
+++ src/WeatherStation/WeatherApplicationRunner.java
@@ -1,5 +1,6 @@
+package WeatherStation;
+
 import CurrentWeather.WeatherCat;
-import WeatherStation.FindWeatherStation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
